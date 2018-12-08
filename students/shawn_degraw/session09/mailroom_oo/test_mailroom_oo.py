119d725b3a7edfd2de92143bfe4020b7a202868c
motians <motians@comcast.net>
2018-12-08 13:18:58 -0800

    added tests after refactor

--- students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
+++ students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
@@ -7,11 +7,11 @@ from mailroom_oo.donor_models import *
 
 def test_donor_init():
 
-    d1 = Donor("Shawn DeGraw")
+    d1 = Donor("Shawn DeGraw", 1000)
     d2 = Donor("Brandon Nguyen", 5000)
 
     assert d1.name == "Shawn DeGraw"
-    assert d1.donations == []
+    assert d1.donations == [1000]
 
     assert d2.name == "Brandon Nguyen"
     assert d2.donations == [5000]
@@ -19,31 +19,25 @@ def test_donor_init():
 
 def test_donor_manipulation():
 
-    d1 = Donor("Shawn DeGraw")
+    d1 = Donor("Shawn DeGraw", 2000)
 
     d1.add_donation("Shawn DeGraw", 5000)
-    d1.add_donation("Shawn DeGraw", 2000)
     d1.add_donation("Shawn DeGraw", 3000)
 
-    assert d1.donations == [5000, 2000, 3000]
+    assert d1.donations == [2000, 5000, 3000]
     assert d1.number_donations == 3
     assert round(d1.average_donation, 2) == 3333.33
 
 
 def test_donorcollection():
 
-    dc1 = DonorCollection()
     dc2 = DonorCollection()
 
-    dc1.donor_update("John Doe")
-    dc2.donor_update("Jane Doe", 2000)
+    dc2.donor_update("Jane Doe", 20.00)
 
-    assert dc1.search_name("John Doe").name == "John Doe"
-    assert dc1.search_name("John Doe").donations == []
     assert dc2.search_name("Jane Doe").name == "Jane Doe"
     assert dc2.search_name("Jane Doe").donations == [2000]
 
-    assert dc2.donor_namelist() == ['Jane Doe']
     assert dc2.collect_data() == ['Jane Doe']
 
     assert dc2.create_report() == 'Donor Name                | Total Given | Num Gifts | Average Gift \nJane Doe                   $      20.00           1  $       20.00'
@@ -55,10 +49,21 @@ def test_donorcollection_multi():
 
     dc1 = DonorCollection()
 
-    dc1.donor_update("Zoe", 100)
-    dc1.donor_update("Sent", 150)
-    dc1.donor_update("Beta", 200)
-    dc1.donor_update("Alpha", 10)
-    dc1.donor_update("Gamma", 80)
+    dc1.donor_update("Zoe", 100.00)
+    dc1.donor_update("Sent", 150.00)
+    dc1.donor_update("Sent", 200.00)
+    dc1.donor_update("Beta", 200.00)
+    dc1.donor_update("Alpha", 10.00)
+    dc1.donor_update("Gamma", 80.00)
 
     assert dc1.collect_data() == ['Alpha', 'Beta', 'Gamma', 'Sent', 'Zoe']
+    assert dc1.search_name(dc1.collect_data()[0]).donations == [1000]
+    assert dc1.search_name(dc1.collect_data()[3]).donations == [15000, 20000]
+
+
+def test_invalid_donationentry():
+
+    dc1 = DonorCollection()
+
+    assert not dc1.donor_update("test")
+    assert not dc1.donor_update("test", "stringvalue")
