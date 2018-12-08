787cd385b99e903b071e41349e01f330a62ab8f2
motians <motians@comcast.net>
2018-12-07 18:33:51 -0800

    tests for changes

--- students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
+++ students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
@@ -7,21 +7,22 @@ from mailroom_oo.donor_models import *
 
 def test_donor_init():
     d1 = Donor("Shawn DeGraw")
-    d2 = Donor("Brandon Nguyen", 50.00)
+    d2 = Donor("Brandon Nguyen", 5000)
 
     assert d1.name == "Shawn DeGraw"
     assert d1.donations == []
 
     assert d2.name == "Brandon Nguyen"
-    assert d2.donations == [50.00]
+    assert d2.donations == [5000]
 
 
 def test_donor_manipulation():
     d1 = Donor("Shawn DeGraw")
 
-    d1.add_donation(50.00)
-    d1.add_donation(20.00)
-    d1.add_donation(30.00)
+    d1.add_donation("Shawn DeGraw", 5000)
+    d1.add_donation("Shawn DeGraw", 2000)
+    d1.add_donation("Shawn DeGraw", 3000)
 
-    assert d1.donations == [50.00, 20.00, 30.00]
+    assert d1.donations == [5000, 2000, 3000]
     assert d1.number_donations == 3
+    assert round(d1.average_donation, 2) == 3333.33
