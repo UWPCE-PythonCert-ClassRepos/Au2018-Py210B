7819e775026f79a39c2aa20c77a851a2c854b235
motians <motians@comcast.net>
2018-12-09 19:29:41 -0800

    most looks good

--- students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
+++ students/shawn_degraw/session09/mailroom_oo/test_mailroom_oo.py
@@ -23,11 +23,28 @@ def test_donor_manipulation():
     d1 = Donor("Shawn DeGraw", 2000)
 
     d1.add_donation("Shawn DeGraw", 5000)
-    d1.add_donation("Shawn DeGraw", 3000)
+    d1.add_donation("Shawn DeGraw", 8000)
 
-    assert d1.donations == [2000, 5000, 3000]
+    assert d1.donations == [2000, 5000, 8000]
     assert d1.number_donations == 3
-    assert round(d1.average_donation, 2) == 3333.33
+    assert round(d1.average_donation, 2) == 5000.0
+
+
+def test_invalid_donationentry():
+
+    dc1 = DonorCollection()
+
+    assert not dc1.donor_update("test")
+    assert not dc1.donor_update("test", "stringvalue")
+
+
+def test_invalid_nameentry():
+
+    dc2 = DonorCollection()
+    dc2.donor_update("  Test   User  ", 20)
+
+    assert dc2.search_name("Test User")
+    assert dc2.search_name("  Test    User  ")
 
 
 def test_donorcollection():
@@ -54,7 +71,7 @@ def test_report():
     assert dc2.report_header() == '\nDonor Name                | Total Given | Num Gifts | Average Gift \n------------------------------------------------------------------'
 
 
-def test_donorcollection_multi():
+def test_donorcollection_sort():
 
     dc1 = DonorCollection()
 
@@ -68,20 +85,3 @@ def test_donorcollection_multi():
     assert dc1.collect_data() == ['Alpha', 'Beta', 'Gamma', 'Sent', 'Zoe']
     assert dc1.search_name(dc1.collect_data()[0]).donations == [1000]
     assert dc1.search_name(dc1.collect_data()[3]).donations == [15000, 20000]
-
-
-def test_invalid_donationentry():
-
-    dc1 = DonorCollection()
-
-    assert not dc1.donor_update("test")
-    assert not dc1.donor_update("test", "stringvalue")
-
-
-def test_invalid_nameentry():
-
-    dc2 = DonorCollection()
-    dc2.donor_update("  Test   User  ", 20)
-
-    assert dc2.search_name("Test User")
-    assert dc2.search_name("  Test    User  ")
